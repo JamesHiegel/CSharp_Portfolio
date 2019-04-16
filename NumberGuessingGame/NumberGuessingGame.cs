b8a124de1dbcf15f7cae864b6a1b4fcf2bad0056
James Hiegel <jameshiegel@outlook.com>
2019-04-16 10:40:40 -0700

    added HigherOrLower method

--- NumberGuessingGame/NumberGuessingGame.cs
+++ NumberGuessingGame/NumberGuessingGame.cs
@@ -18,6 +18,15 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
+            int min = 0;
+            int max = 100;
+
+            int NumToGuess = RandomInt(min, max);
+
+            do
+            {
+
+            } while (false);
         }
 
         // This method generates a random integer between two provided integers, inclusive
@@ -25,6 +34,17 @@ namespace NumberGuessingGame
         {
             Random r = new Random();
             return (r.Next(min, max));
+        }
+
+        // This method compares two provided integers and returns an integer if the first is higher, lower or the same as the second
+        private static int HigherOrLower(int firstNum, int secondNum)
+        {
+            if (firstNum > secondNum)
+                return 1;
+            else if (firstNum < secondNum)
+                return -1;
+            else
+                return 0;
         }
     }
 }
