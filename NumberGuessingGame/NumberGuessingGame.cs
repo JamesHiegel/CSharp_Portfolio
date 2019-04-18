96c94a15bc65379e03f70fe9fe69ac65aad37147
James Hiegel <jameshiegel@outlook.com>
2019-04-18 09:31:51 -0700

    added comments to nested do-while loops

--- NumberGuessingGame/NumberGuessingGame.cs
+++ NumberGuessingGame/NumberGuessingGame.cs
@@ -30,22 +30,29 @@ namespace NumberGuessingGame
                 do
                 {
                     Console.Write("Enter a number between 1 and 100 (0 to quit):");
+
                     int.TryParse(Console.ReadLine(), out input);
 
                     if (input == 0)
-                        return;
+                        return; // exits inner do-while loop when user inputs 0
                     else
                     {
+                        // calls a method to tell the user if a number is higher or lower
+                        // does not output anything if guess is correct
                         HigherOrLower(input, NumToGuess);
+                        // each guess increases the guess count 
                         count++;
                     }
 
-                } while (input != NumToGuess);
+                } while (input != NumToGuess); // loops until the user inputs the correct number
 
                 Console.WriteLine("You guessed it! The number was {0}!", NumToGuess);
                 Console.WriteLine("It took you {0} {1}.\n", count, count == 1 ? "try" : "tries");
 
-            } while (input != 0);
+                count = 0; // resets guess counter
+
+            } while (input != 0); // loops until user inputs 0
+
         } // end Main()
 
         // This method returns a randomly generated integer between two provided integers, inclusive.
