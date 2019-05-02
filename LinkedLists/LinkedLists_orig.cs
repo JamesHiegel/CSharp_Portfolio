b9e1bae65026d5f63b742337fe6dc4c4d3ce8e72
James Hiegel <jameshiegel@outlook.com>
2019-05-02 08:07:54 -0700

    added linkedlist project

---
+++ LinkedLists/LinkedLists_orig.cs
@@ -0,0 +1,31 @@
+﻿// SOURCE: https://www.c-sharpcorner.com/article/linked-list-implementation-in-c-sharp/
+// AUTHOR: Ankit Sharma,  Senior Software Engineer currently working with IVY Comptech in Hyderabad, 
+// India. He writes articles for multiple platforms, which includes c-sharpcorner, Dzone, Medium and 
+// TechNet Wiki. For his dedicated contribution to the developer’s community, he has been recognized 
+// as c-sharpcorner MVP, Dzone MVB and Top contributor in Technology at Medium.
+
+// FILENAME: LinkedLists_orig.cs
+// PURPOSE: Demonstrate how a linked list and doubly linked list works.
+// STUDENT: James Hiegel
+// DATE: 02 May 2019
+
+// STYLE MODIFICATIONS: none
+
+// FUNCTIONAL MODIFICATIONS: none
+
+/*
+* A C# program that implements a linked list and doubly linked 
+* list and demonstrates various operations on those lists. 
+*/
+using System;
+
+namespace LinkedLists
+{
+    class LinkedLists_orig
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
