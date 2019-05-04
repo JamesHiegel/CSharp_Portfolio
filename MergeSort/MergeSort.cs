05b655a70883b3749f015d5097c590b58ebdf95f
James Hiegel <james.hiegel@gmail.com>
2019-05-04 16:10:47 -0700

    added Merge method

--- MergeSort/MergeSort.cs
+++ MergeSort/MergeSort.cs
@@ -70,5 +70,39 @@ namespace MergeSort
             right = MergeSort(right);
             return Merge(left, right);
         }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
     }
 }
