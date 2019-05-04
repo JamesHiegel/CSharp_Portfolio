4a0feccad69bb60ae3594e3089d9b138f01dcabf
James Hiegel <james.hiegel@gmail.com>
2019-05-04 15:42:36 -0700

    added main method code

--- MergeSort/MergeSort_orig.cs
+++ MergeSort/MergeSort_orig.cs
@@ -22,7 +22,27 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Input int array : ");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(numbers[k] + " ");
+                Console.Write("\n");
+            }
+            Console.WriteLine("MergeSort By Recursive Method");
+            SortMerge(numbers, 0, max - 1);
+            for (int i = 0; i < max; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadLine();
         }
 
         static public void MainMerge(int[] numbers, int left, int mid, int right)
