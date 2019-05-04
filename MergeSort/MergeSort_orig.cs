a2a0c980d7c4aacd33abf7ff2643d6d1c344e51f
James Hiegel <james.hiegel@gmail.com>
2019-05-04 15:41:09 -0700

    added header comments

--- MergeSort/MergeSort_orig.cs
+++ MergeSort/MergeSort_orig.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// SOURCE: https://www.c-sharpcorner.com/blogs/merge-sorting-algorithm-in-c-sharp1
+// AUTHOR: Abhay Shanker
+
+// FILENAME: MergeSort_orig.cs
+// PURPOSE: Demonstrate a merge sort on an integer array.
+// STUDENT: James Hiegel
+
+// STYLE MODIFICATIONS: 
+
+// FUNCTIONAL MODIFICATIONS:
+
+
+// Merge sort is based on the divide-and-conquer paradigm. We break down
+// an array into two sub arrays. This code sample explains how a merge 
+// sort algorithm works and how it is implemented in C#.
+
+using System;
 
 namespace MergeSort
 {
@@ -8,5 +24,7 @@ namespace MergeSort
         {
             Console.WriteLine("Hello World!");
         }
+
+
     }
 }
