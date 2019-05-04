996b87482a726e49e7fa48bdc2d6ef78114b373e
James Hiegel <james.hiegel@gmail.com>
2019-05-04 16:09:57 -0700

    main method code

--- MergeSort/MergeSort.cs
+++ MergeSort/MergeSort.cs
@@ -17,6 +17,7 @@
 // This will be the sorted list.
 
 using System;
+using System.Collections.Generic;
 
 namespace MergeSort
 {
@@ -24,7 +25,28 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-           
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            Random random = new Random();
+
+            Console.WriteLine("Original array elements:");
+            for (int i = 0; i < 10; i++)
+            {
+                unsorted.Add(random.Next(0, 100));
+                Console.Write(unsorted[i] + " ");
+            }
+            Console.WriteLine();
+
+            sorted = MergeSort(unsorted);
+
+            Console.WriteLine("Sorted array elements: ");
+            foreach (int x in sorted)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("\n");
         }
 
+    }
 }
