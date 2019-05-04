7c96e1d7241cbf639b316c14dab607110d270163
James Hiegel <james.hiegel@gmail.com>
2019-05-04 15:41:48 -0700

    added MainMerge method

--- MergeSort/MergeSort_orig.cs
+++ MergeSort/MergeSort_orig.cs
@@ -25,6 +25,30 @@ namespace MergeSort
             Console.WriteLine("Hello World!");
         }
 
+        static public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
 
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
     }
 }
