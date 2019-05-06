6de66f36dbbce06e32b42ecd12097aaa64f55556
James Hiegel <jameshiegel@outlook.com>
2019-05-06 14:58:46 -0700

    binary search tree shell and header comments

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,33 @@
+﻿// SOURCE: https://www.sanfoundry.com/csharp-program-binary-search-tree-linked-list/
+// AUTHOR: Manish Bhojasia, a technology veteran with 20+ years @ Cisco & Wipro, is 
+// Founder and CTO at Sanfoundry. He is Linux Kernel Developer & SAN Architect and is 
+// passionate about competency developments in these areas. He lives in Bangalore 
+// and delivers focused training sessions to IT professionals in Linux Kernel, Linux
+// Debugging, Linux Device Drivers, Linux Networking, Linux Storage, Advanced C 
+// Programming, SAN Storage Technologies, SCSI Internals & Storage Protocols such as 
+// iSCSI & Fiber Channel.
+
+// FILENAME: BinarySearchTree.cs
+// PURPOSE: 
+// STUDENT: James Hiegel
+
+// STYLE MODIFICATIONS: 
+
+
+// FUNCTIONAL MODIFICATIONS:
+
+
+
+
+using System;
+
+namespace JJH
+{
+    class BinarySearchTree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
