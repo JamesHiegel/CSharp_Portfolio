c2a32f46366b02d9b3fee7a3f19ce1bf12aa7064
James Hiegel <james.hiegel@gmail.com>
2019-05-06 10:43:41 -0700

    main method

--- QuickSort/QuickSort.cs
+++ QuickSort/QuickSort.cs
@@ -6,7 +6,26 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            Quick_Sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
         }
     }
+}
 }
