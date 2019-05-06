5faa6d5533297ff1457bfd2b0ee62d2dc6ed9011
James Hiegel <jameshiegel@outlook.com>
2019-05-06 13:48:07 -0700

    new program shell

---
+++ StackAsArray/README.md
@@ -0,0 +1,22 @@
+# Queue implemented as Linked List
+
+Example program that utilizes an array to implement a stack data structure.
+
+## Getting Started
+
+Download, and then compile and run in your favorite editor.
+
+## 
+![]()
+
+## 
+![]()
+
+## Authors
+
+* **Ankit Sharma** - *Original author*
+* **James Hiegel** - *Added comments and additional functionality to original program*
+
+## Source
+
+https://www.geeksforgeeks.org/implementing-stack-c-sharp/
---
+++ StackAsArray/StackAsArray.cs
@@ -0,0 +1,25 @@
+﻿// SOURCE: https://www.geeksforgeeks.org/implementing-stack-c-sharp/
+// AUTHORS: Ankit Sharma
+
+// FILENAME: StackAsArray.cs
+// PURPOSE: Implement a stack utilizing an array data structure.
+// STUDENT: James Hiegel
+
+// STYLE MODIFICATIONS: 
+
+// FUNCTIONAL MODIFICATIONS:
+
+//
+
+using System;
+
+namespace JJH
+{
+    class StackAsArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
