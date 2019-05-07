36dd8910030d6d98bbbcfe5c02f5ec3646442a04
James Hiegel <james.hiegel@gmail.com>
2019-05-06 18:58:50 -0700

    hello world and header comments

---
+++ ReverseArray/ReverseArray.cs
@@ -0,0 +1,24 @@
+﻿// SOURCE: hhttps://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+// AUTHORS: Sam007, ChitraNayal, pp_pankaj
+
+// FILENAME: ReverseArray.cs
+// PURPOSE: Given an array (or string), the task is to reverse the array/string.
+// STUDENT: James Hiegel
+
+// STYLE MODIFICATIONS: 
+
+// FUNCTIONAL MODIFICATIONS:
+
+// C# Program that reverses a provided array.
+using System;
+
+namespace JJH
+{
+    class ReverseArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
