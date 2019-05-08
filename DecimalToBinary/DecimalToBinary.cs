e3c2e069f5b69cb10dec8d97ce75ab232b51ac87
James Hiegel <jameshiegel@outlook.com>
2019-05-08 15:03:49 -0700

    added decimal to binary program

---
+++ DecimalToBinary/DecimalToBinary.cs
@@ -0,0 +1,56 @@
+﻿// SOURCE: https://www.sanfoundry.com/csharp-program-convert-decimal-binary/
+// AUTHOR: Manish Bhojasia
+// FILENAME: DecimalToBinary.cs
+// PURPOSE: C# Program to Convert a Given Number of Days in terms of Years, Weeks & Days.
+// STUDENT: James Hiegel
+// DATE: 08 May 2019
+
+// STYLE MODIFICATIONS:
+// Added comments to classes, methods and code blocks clarifying what the program is doing
+// Used blank lines to make program easier to read
+
+// FUNCTIONAL MODIFICATIONS:
+// Added extra Console.WriteLine telling user what the program does
+
+/*
+ * C# Program to Convert Decimal to Binary
+ */
+using System;
+
+namespace JJH
+{
+    public class DecimalToBinary
+    {
+        public static void Main()
+        {
+            int num;
+            int quot;
+            string rem = "";
+
+            Console.WriteLine("This program converts an integer to a binary number.");
+            // collects user input
+            Console.Write("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+
+            // divides number by two and then saves remainder
+            // repeats process until number is less than 1
+            while (num >= 1)
+            {
+                quot = num / 2;
+                rem += (num % 2).ToString();
+                num = quot;
+            }
+
+            // reverses order of rem string to form binary number
+            string bin = "";
+            for (int i = rem.Length - 1; i >= 0; i--)
+            {
+                bin = bin + rem[i];
+            }
+
+            // displays binary number
+            Console.WriteLine("The Binary format for given number is {0}", bin);
+            Console.Read();
+        }
+    }
+}
---
+++ DecimalToBinary/README.md
@@ -0,0 +1,19 @@
+# Convert Decimal to Binary
+
+Example program that Converts Decimal to Binary.
+
+## Getting Started
+
+Download, and then compile and run in your favorite editor.
+
+## Decimal to Binary
+![Decimal to Binary](https://github.com/JamesHiegel/CSharp_Portfolio/blob/master/DecimalToBinary/img/running.PNG)
+
+## Authors
+
+* **Manish Bhojasia** - *Original Author*
+* **James Hiegel** - *Added comments and additional functionality to original program*
+
+## Source
+
+https://www.sanfoundry.com/csharp-program-convert-decimal-binary/
