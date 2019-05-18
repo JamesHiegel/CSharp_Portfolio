ef45e7c8cedb9fcfa6ceb73a68e3e408d838d82e
James Hiegel <james.hiegel@gmail.com>
2019-05-18 07:39:57 -0700

    added CountVowels method

---
+++ CountVowelsInStringLINQ/CountVowelsInStringLINQ.cs
@@ -0,0 +1,37 @@
+﻿// AUTHOR: James Hiegel
+// FILENAME: CountVowelsInStringLINQ.cs
+// PURPOSE: Count the number of vowels in a string, both uppercase and lowercase.
+// DATE: 18 May 2019
+
+// This program prompts a user to enter a sentence and then counts the 
+// uppercase and lowercase vowels in the sentence. The program then 
+// outputs the number of vowels
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JJH
+{
+    public class CountVowelsInStringLINQ
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This program returns the number of vowels in a sentence.");
+        }
+    }
+
+    public class Utility
+    {
+        public static int CountVowels(string input)
+        {
+            char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
+
+            IEnumerable<char> query = from c in input.ToLower()
+                                      where VOWELS.Contains(c)
+                                      select c;
+
+            return query.Count();
+        }
+    }
+}
