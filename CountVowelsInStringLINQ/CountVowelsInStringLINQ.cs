82e22921156992048cd6230aea45e4d3209d9fb4
James Hiegel <james.hiegel@gmail.com>
2019-05-18 08:04:14 -0700

    added comments to program

--- CountVowelsInStringLINQ/CountVowelsInStringLINQ.cs
+++ CountVowelsInStringLINQ/CountVowelsInStringLINQ.cs
@@ -13,44 +13,46 @@ using System.Linq;
 
 namespace JJH
 {
+    // driver class
     public class CountVowelsInStringLINQ
     {
+        // driver method
         public static void Main(string[] args)
         {
             Utility.Menu();
         }
     }
 
-
+    // The Utility class contains a method to display the menu
+    // and a method to count the number of vowels in a string
     public class Utility
     {
-        public static int CountVowels(string input)
-        {
-            char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
-
-            IEnumerable<char> query = from c in input.ToLower()
-                                      where VOWELS.Contains(c)
-                                      select c;
-
-            return query.Count();
-        }
-
+        // The Menu method displays a menu for the user and then
+        // prompts the user to enter a string. The method then
+        // calls the CountVowels method to return the number of 
+        // vowels in the sentence.
         public static void Menu()
         {
             string input;
 
+            // loops until user answers no to run again
             do
             {
+                // requests user to enter a sentence for counting
                 Console.WriteLine("This program returns the number of vowels in a sentence.");
                 Console.WriteLine("Enter a sentence to be counted:");
                 input = Console.ReadLine();
 
                 Console.WriteLine();
 
+                // displays the number of characters and vowels in the string
+                // calls the CountVowels method to obtain the number of vowels
                 Console.WriteLine("There are {0} characters in the sentence, and {1} vowels.", input.Length, Utility.CountVowels(input));
 
                 Console.WriteLine();
 
+                // asks the user if they want to run the counting algorithm again
+                // repeats the question until the user enters valid input
                 do
                 {
                     Console.Write("Run again? (y/n):");
@@ -60,6 +62,26 @@ namespace JJH
                 Console.WriteLine();
 
             } while (input == "y");
+        }
+
+        // The CountVowels method returns the number of vowels (A, E, I, O, U)
+        // in a provided string using a LINQ query.
+        public static int CountVowels(string input)
+        {
+            // list of vowels, does NOT include Y
+            char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
+
+            // creates a LINQ query
+            // 1) converts string to all lowercase, then iterates over the string
+            // 2) checks if each char is a vowel
+            // 3) selects only the char's that are vowels
+            IEnumerable<char> query = from c in input.ToLower()
+                                      where VOWELS.Contains(c)
+                                      select c;
+
+            // since the query only contains vowels returning the 
+            // count gives the number of vowels in the string
+            return query.Count();
         }
     }
 }
