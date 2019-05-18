b1dfff44bce016879d35a994f225a469f12f92dc
James Hiegel <james.hiegel@gmail.com>
2019-05-18 08:54:26 -0700

    added Menu and RunAgain methods and shell for CheckPositiveInteger method

--- PositiveInteger/PositiveInteger.cs
+++ PositiveInteger/PositiveInteger.cs
@@ -15,12 +15,76 @@ namespace JJH
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool opt;
+
+            do
+            {
+                Utility.Menu();
+                opt = Utility.RunAgain();
+
+            } while (opt);
         }
     }
 
     public class Utility
     {
+
+        public static void Menu()
+        {
+            const int THREE = 3;
+
+            Console.WriteLine("This program asks for a postive integer and then checks " +
+                "that the input is valid and then determines if it is divisible by 3.");
+
+            Console.WriteLine();
+
+            Console.Write("Enter a positive integer: ");
+            string input = Console.ReadLine();
+
+            int number = CheckPositiveInteger(input);
+
+            Console.Write("The number {0} is ", input);
+
+            if (!CheckDivisionNoRemainder(number, THREE))
+                Console.Write("NOT ");
+
+            Console.WriteLine("divible by {0} with no remainder.", THREE);
+
+        }
+
+        public static bool RunAgain()
+        {
+            string input;
+            bool ret = false;
+
+            do
+            {
+                Console.Write("Run Again? (y/n): ");
+                input = Console.ReadLine();
+            } while (!(input == "y" || input == "n"));
+
+            if (input == "y")
+                ret = true;
+
+            return ret;
+        }
+
+        public static int CheckPositiveInteger(string input)
+        {
+            int result = 0;
+
+            try
+            {
+                result = Int32.Parse(input);
+            }
+            catch
+            {
+
+            }
+
+            return result;
+        }
+
         // The CheckDivisionNoRemainder method returns true if the provided numerator 
         // can be divided by the denominator with no remainder, otherwise the method 
         // returns false. The method throws an exception if denominator is zero.
