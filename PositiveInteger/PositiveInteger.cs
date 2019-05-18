20b054fdf74fbbf243d2f95dd820c30b50eed75e
James Hiegel <james.hiegel@gmail.com>
2019-05-18 08:27:51 -0700

    default main method with program description

---
+++ PositiveInteger/PositiveInteger.cs
@@ -0,0 +1,23 @@
+﻿// AUTHOR: James Hiegel
+// FILENAME: PositiveInteger.cs
+// PURPOSE: Checks if an entered integer is positive and then checks if it is divisble by 3.
+// DATE: 18 May 2019
+
+// This program prompts a user to enter an positive integer and then
+// checks to ensure it is valid. It throws and handles an exception
+// if the input is invalid. Lastly it checks if the integer is 
+// divisible by 3.
+using System;
+
+namespace JJH
+{
+    public class PositiveInteger
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}
